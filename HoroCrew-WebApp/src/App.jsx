e2624d5b1d3fdbbe29a5610908915a1e7586f21f
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2023-04-02 17:27:11 +0200

    feat sign up form

--- HoroCrew-WebApp/src/App.jsx
+++ HoroCrew-WebApp/src/App.jsx
@@ -1,7 +1,8 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import Navbar from "./components/Misc/Navbar/Navbar";
-import { Login } from "./views/Login/Login";
-import { Home } from "./views/HomePage/Home";
+import Login from "./views/misc/Login";
+import { Home } from "./views/misc/Home";
+import { Navbar } from "./components/Misc/Navbar/Navbar";
+import FormSignUp from "./views/misc/Signup";
 
 
 
@@ -13,9 +14,9 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />}>
-            <Route path='*' element={<Navigate to='/' />} />
-          </Route>
+          <Route path="/signup" element={<FormSignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='*' element={<Navigate to='/' />} />
         </Routes>
 
       </div>
---
+++ HoroCrew-WebApp/src/views/misc/Signup.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import { Formik, Form, ErrorMessage } from "formik";
+import TextField from "../../components/TextField/TextField";
+import * as Yup from "yup";
+import { SignUpSchema } from "../../utils/schemas/signUp.schema";
+
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    age: "",
+    confirmPassword: "",
+    date: "",
+};
+
+export default function signUp() {
+
+    return (
+        <div>
+            <Formik
+                initialValues={initialValues}
+                validateOnBlur={true}
+                validateOnChange={false}
+                validationSchema={SignUpSchema}
+                onSubmit={(values) => {
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
+                {(formik) => (
+                    <div>
+                        <h1 className="">Signup</h1>
+                        <Form className="form p-3">
+                            <div>
+                                <h2> Essencial Information</h2>
+
+                                <TextField
+                                    type="text"
+                                    label="Firstname"
+                                    name="firstName"
+                                    placeholder="Your First name here"
+                                />
+                                <TextField
+                                    type="text"
+                                    name="lastName"
+                                    label="Lastname"
+                                    placeholder="Your Last name here"
+                                />
+                                <TextField
+                                    type="email"
+                                    name="email"
+                                    label="Email"
+                                    placeholder="example@example.com"
+                                />
+
+
+                            </div>
+
+                            <div> <h2>Date Of Birth</h2>
+                                <TextField
+                                    type="number"
+                                    name="dayOfBirth"
+                                    label="Day"
+                                    placeholder="DD"
+                                />
+                                <TextField
+                                    type="number"
+                                    name="monthOfBirth"
+                                    label="Month"
+                                    placeholder="MM"
+                                />
+                                <TextField
+                                    type="number"
+                                    name="yearOfBirth"
+                                    label="Year"
+                                    placeholder="YYYY"
+                                />
+                            </div>
+                            <div> <h2>Time Of Birth</h2>
+                                <TextField
+                                    type="time"
+                                    name="timeOfBirth"
+                                    label="time"
+                                    placeholder="00:00"
+                                />
+
+                            </div>
+
+                            <div>
+                                <h2> Password </h2>
+                                <TextField
+                                    type="text"
+                                    name="password"
+                                    label="Password"
+                                    placeholder="****"
+                                />
+                                <div className="mb-2">
+                                    <label htmlFor="confirmPassword">Confirm Password</label>
+                                    <input
+                                        id="confirmPassword"
+                                        className={`form-control shadow-none ${formik.touched.confirmPassword &&
+                                            formik.errors.confirmPassword &&
+                                            "is-invalid"
+                                            }`}
+                                        type="text"
+                                        name="confirmPassword"
+                                        placeholder="confirm password..."
+                                        {...formik.getFieldProps("confirmPassword")}
+                                    />
+                                    <ErrorMessage
+                                        component="div"
+                                        name="confirmPassword"
+                                        className="error"
+                                    />
+                                </div>
+                            </div>
+
+                            <button className="btn btn-dark m-3" type="submit">
+                                Register
+                            </button>
+                            <button className="btn btn-primary m-3" type="reset">
+                                Reset
+                            </button>
+                        </Form>
+                    </div>
+                )
+                }
+            </Formik >
+        </div >
+    );
+}
+
