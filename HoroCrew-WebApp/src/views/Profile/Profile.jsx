0353479a349fcb530d26d0cbb43e87f652856fe7
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2023-04-03 19:16:11 +0200

    feat profile

--- HoroCrew-WebApp/src/views/Profile/Profile.jsx
+++ HoroCrew-WebApp/src/views/Profile/Profile.jsx
@@ -1,4 +1,11 @@
-export const Profile = () => {
-    return (<h1> HOLA </h1>)
-}
+import { useContext } from "react";
+import AuthContext from "../../context/AuthContext";
 
+export const Profile = () => {
+    const { getCurrentUser } = useContext(AuthContext)
+    return (
+        <div>
+            <h1> Profile of {getCurrentUser.firstName} {getCurrentUser.lastName} </h1>
+        </div>
+    )
+}
