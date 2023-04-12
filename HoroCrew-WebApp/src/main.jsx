dd797361a958d2cee57f213240570f0c7a72bc4b
pianattero <mariapianattero@gmail.com>
2023-04-12 17:34:42 +0200

    change folder org

--- HoroCrew-WebApp/src/main.jsx
+++ HoroCrew-WebApp/src/main.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import "./main.css"
-import { BrowserRouter } from "react-router-dom"
-import { AuthProvider } from './context/AuthContext'
-import { IconContext } from 'react-icons'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./main.css";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
   </BrowserRouter>
-)
+);
